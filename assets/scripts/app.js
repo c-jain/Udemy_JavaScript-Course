49e510972a31ce93b73f20a431033550e7db6d44
c-jain <chakshujain1997@gmail.com>
2020-08-22 21:33:07 +0530

    Section 3 complete

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,65 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
-currentResult = (currentResult + 10) * 3 / 2 - 1;
+function getUserNumberInput() {
+  return parseInt(userInput.value);
+}
 
-let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+function createAndWriteOutptu(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription);
+}
 
-outputResult(currentResult, calculationDescription);
+function writetoLog(
+  operationIdentifier,
+  prevResult,
+  operationNumber,
+  newResult
+) {
+  const logEntry = {
+    operation: operationIdentifier,
+    prevResult: prevResult,
+    number: operationNumber,
+    result: newResult,
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
+}
+
+function add() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult + enteredNumber;
+  createAndWriteOutptu('+', initialResult, enteredNumber);
+  writetoLog('ADD', initialResult, enteredNumber, currentResult);
+}
+
+function subtract() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult - enteredNumber;
+  createAndWriteOutptu('-', initialResult, enteredNumber);
+  writetoLog('SUBTRACT', initialResult, enteredNumber, currentResult);
+}
+
+function multiply() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult * enteredNumber;
+  createAndWriteOutptu('*', initialResult, enteredNumber);
+  writetoLog('MULTIPLY', initialResult, enteredNumber, currentResult);
+}
+
+function divide() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult / enteredNumber;
+  createAndWriteOutptu('/', initialResult, enteredNumber);
+  writetoLog('DIVIDE', initialResult, enteredNumber, currentResult);
+}
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
